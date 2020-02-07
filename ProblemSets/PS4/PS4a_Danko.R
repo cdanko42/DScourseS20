1d2cdc8dc40bf44e64aeba956752e15c750a1fc8
cdanko42 <christopher.p.danko-1@ou.edu>
2020-02-06 18:52:07 -0600

    Completed Problem Set 4

---
+++ ProblemSets/PS4/PS4a_Danko.R
@@ -0,0 +1,7 @@
+system("wget -O nfl.json 'http://api.fantasy.nfl.com/v1/players/stats?statType=seasonStats&season=2010&week=1&format=json'")
+system("cat nfl.json")
+library(jsonlite)
+df <- fromJSON('nfl.json')
+class(df)
+class(df$players)
+head(df$players,10)
